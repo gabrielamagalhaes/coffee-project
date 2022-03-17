fad306e34e5b52638f3e41ceac252bcfb140569c
Gabriela Pereira <gabrielaprg17@gmail.com>
2022-03-16 22:37:09 -0300

    adds delete and get methods

--- lib/coffee_project.ex
+++ lib/coffee_project.ex
@@ -2,6 +2,13 @@ defmodule CoffeeProject do
   @kinds [:arabica, :conilon]
   @types [:grão, :moido, :solúvel]
 
+  @fake_coffee %{
+    kind: :conilon,
+    brand: "banana coffee",
+    type: :moido,
+    id: 28
+  }
+
   def create(%{kind: kind, brand: _brand, type: type})
       when type in @types and kind in @kinds do
     {:ok, %{message: "Coffee created", id: Enum.random(0..100), status: :ok}}
@@ -9,5 +16,21 @@ defmodule CoffeeProject do
 
   def create(_params) do
     {:error, %{message: "Invalid params", status: :bad_request}}
+  end
+
+  def delete(28) do
+    {:ok, %{message: "Coffee deleted", status: :ok, coffee: @fake_coffee}}
+  end
+
+  def delete(_id) do
+    {:error, %{message: "Coffee not found", status: :not_found}}
+  end
+
+  def get(28) do
+    {:ok, %{status: :ok, coffee: @fake_coffee}}
+  end
+
+  def get(_id) do
+    {:error, %{message: "Coffee not found", status: :not_found}}
   end
 end
