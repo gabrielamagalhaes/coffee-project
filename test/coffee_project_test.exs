350306be5b65baecdd1ae03321a8fd9e691653c3
Gabriela Pereira <gabrielaprg17@gmail.com>
2022-03-20 19:57:50 -0300

    adds delete and get success tests

--- test/coffee_project_test.exs
+++ test/coffee_project_test.exs
@@ -1,5 +1,13 @@
 defmodule CoffeeProjectTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
+
+  setup do
+    id = 28
+
+    coffee = %{brand: "banana coffee", id: 28, kind: :conilon, type: :moido}
+
+    {:ok, id: id, coffee: coffee}
+  end
 
   describe "create/1" do
     test "when all params are valid returns a successfull response" do
@@ -16,6 +24,37 @@ defmodule CoffeeProjectTest do
       expected_response = {:error, %{message: "Invalid params", status: :bad_request}}
 
       response = CoffeeProject.create(params)
+
+      assert response == expected_response
+    end
+  end
+
+  describe "delete/1" do
+    test "when the coffee exists, deletes the coffee", %{id: id, coffee: coffee} do
+      expected_response =
+        {:ok,
+         %{
+           coffee: coffee,
+           message: "Coffee deleted",
+           status: :ok
+         }}
+
+      response = CoffeeProject.delete(id)
+
+      assert response == expected_response
+    end
+  end
+
+  describe "get/1" do
+    test "when the coffee exists, get the coffee", %{id: id, coffee: coffee} do
+      expected_response =
+        {:ok,
+         %{
+           coffee: coffee,
+           status: :ok
+         }}
+
+      response = CoffeeProject.get(id)
 
       assert response == expected_response
     end
